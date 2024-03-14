62b984a0eae8b80a6e54d767a36975499b252d2d
mohdfaizanali <faizanali5394@gmail.com>
2024-03-14 12:17:47 +0530

    deleting existing records

--- Task_1.py
+++ Task_1.py
@@ -1,12 +1,20 @@
 import sqlalchemy
 from sqlalchemy import create_engine
 import pandas as pd
+import os
 
-engine = create_engine('postgresql://postgres:Postgres@localhost:5432/demo')
+user = os.environ['postgres_username']
+pwd = os.environ['postgres_password']
+host = os.environ['postgres_host']
+port = os.environ['postgres_port']
+database = os.environ['postgres_database']
 
-file_path = 'C:\\Users\\Faizan Ali\\Downloads\\DATETIME.xlsx'
-df = pd.read_excel(file_path)
+engine = create_engine(f'postgresql://{user}:{pwd}@{host}:{port}/{database}')
 
-df.to_sql("emp_table1",engine,if_exists='replace', index=False)
+file_path1 = os.environ.get('file_path')
+print("File path:", file_path1)  # Print the value of the environmental variable
+
+df = pd.read_excel(file_path1)
+df.to_sql("emp_table6",engine,if_exists='replace', index=False)
 
 print(df)
