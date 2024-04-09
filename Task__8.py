3d6446f29ec68d01c124357ee89026b26bb7e149
mohdfaizanali <faizanali5394@gmail.com>
2024-04-09 16:04:43 +0530

    loading json file to postgres

--- Task__8.py
+++ Task__8.py
@@ -10,31 +10,27 @@ port = os.environ['postgres_port']
 database = os.environ['postgres_database']
 
 # Read JSON file
-def read_json_file(json_file_path):
-    with open(json_file_path, 'r') as f:
+def read_json_file(json_path):
+    with open(json_path, 'r') as f:
         data = json.load(f)
     return data
 
-
 # Connect to PostgreSQL database using SQLAlchemy
-def connect_to_db(dbname, user, password, host, port):
+def connect_to_db(user, pwd, host, port, database):
     engine = create_engine(f'postgresql://{user}:{pwd}@{host}:{port}/{database}')
     return engine
-
 
 # Read data from Excel file using pandas
 def read_excel_file(excel_file_path):
     df = pd.read_excel(excel_file_path)
     return df
 
-
 if __name__ == "__main__":
     # Path to your JSON file
-    json_file_path = "C:\\Users\\Faizan Ali\\Downloads\\test.json"
-
+    json_path = os.environ['json_file_path']
 
     # Read JSON data
-    json_data = read_json_file(json_file_path)
+    json_data = read_json_file(json_path)
 
     # Extract information from JSON
     table_name = json_data[0]['table_name']
@@ -42,7 +38,7 @@ if __name__ == "__main__":
     excel_file_path = json_data[0]['file_path']
 
     # Connect to PostgreSQL database
-    engine = create_engine(f'postgresql://{user}:{pwd}@{host}:{port}/{database}')
+    engine = connect_to_db(user, pwd, host, port, database)
 
     # Read data from Excel file
     df = read_excel_file(excel_file_path)
